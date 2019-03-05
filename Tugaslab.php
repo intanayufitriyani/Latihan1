2231ca24880f23ba052d67f11ccf42308f5ca685
intanayufitriyani <47770464+intanayufitriyani@users.noreply.github.com>
2019-03-05 13:19:17 +0700

    Add files via upload

---
+++ Tugaslab.php
@@ -0,0 +1,20 @@
+<?php
+    $organisasi = array(
+       "keagamaan"     array(
+           "ketua"          "siti",
+           "wakil"          "maryam",
+           "anggota"         "husni"
+       ),
+
+       "kebersihan"     array(
+           "ketua"          "aditya",
+           "wakil"          "",
+           "sekertaris"     "chulis"
+       ),
+
+       "kemanusiaan"        array(
+           "ketua"              "pratama",
+           "wakil"              "nugraha"
+       )
+    );
+?>
